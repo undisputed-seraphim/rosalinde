d4de8ed00fbc3d65f0faf91ace672e5f86468f82
Tan Li Boon <undisputed.seraphim@gmail.com>
2025-01-02 21:20:25 +0800

    Slightly improve texture syntax

--- viewer/src/shader.cpp
+++ viewer/src/shader.cpp
@@ -24,12 +24,10 @@ constexpr std::string_view kf_frag_src = "# version 300 es\n"
 "flat in        int  v_texid;\n"
 "uniform sampler2DArray u_tex;\n"
 "out     highp vec4  fragColor;\n"
-"        highp ivec3 texsize;\n"
-"        highp vec3  texcoord;\n"
 
 "void main(void) {\n"
-"    texsize = textureSize(u_tex, 0);\n"
-"    texcoord = vec3(v_uv.x / float(texsize.x), v_uv.y / float(texsize.y), float(v_texid));\n"
+"    highp ivec3 texsize = textureSize(u_tex, 0);\n"
+"    highp vec3 texcoord = vec3(v_uv.x / float(texsize.x), v_uv.y / float(texsize.y), float(v_texid));\n"
 // x-axis of texture must be shifted by 2 to avoid artifacts.
 "    fragColor = textureOffset(u_tex, texcoord, ivec2(2, 0)) * v_fog;\n"
 "}";
